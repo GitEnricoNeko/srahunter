5b2241b7fdcb4462b2e30b3ba1e8f23bbcba717e
Riccardo-Frizzo <155966437+Riccardo-Frizzo@users.noreply.github.com>
2024-01-23 16:18:42 +0100

    Update pyfiglet_wrapper.py

--- scripts/pyfiglet_wrapper.py
+++ scripts/pyfiglet_wrapper.py
@@ -1,12 +1,1 @@
-#!/usr/bin/env python
-# pyfiglet_wrapper.py
-import pyfiglet
-import sys
-
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        text = ' '.join(sys.argv[1:])
-        print(pyfiglet.figlet_format(text))
-    else:
-        print("Usage: pyfiglet_wrapper.py [text]")
 
