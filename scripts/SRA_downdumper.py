07b2c39b6b04e736ec7759af1ed06f48e38fd3ae
Enrico <75318813+GitEnricoNeko@users.noreply.github.com>
2024-01-25 16:43:19 +0100

    Add files via upload

--- scripts/SRA_downdumper.py
+++ scripts/SRA_downdumper.py
@@ -17,15 +17,28 @@ class ArgumentParserWithErrorHandling(argparse.ArgumentParser):
         self.print_help()
         sys.exit(2)
 
+default_dir = os.path.join(os.getcwd(), 'tmp_srahunter')
 # Argument specification
 parser = ArgumentParserWithErrorHandling(
     description='SRAHunter: Download and dump files using an accession list from SRA')
-parser.add_argument('--list', help='Accession list from SRA (file path)', required=True)
+parser.add_argument('--list','-l', help='Accession list from SRA (file path)', required=True)
 parser.add_argument('-t', type=int, default=6, help='Number of threads (default: 6)')
-parser.add_argument('--path', default='~/ncbi/public/sra/', help='Path to SRA folder (default: ~/ncbi/public/sra/)')
-parser.add_argument('--maxsize', default="50G", help='Max size of each fastq file (default: 50G)')
+parser.add_argument('--path', '-sra-path','-p', default=default_dir, help='Path to where to download .sra files (default: currentdirectory/tmp_srahunter')
+parser.add_argument('--maxsize','-ms',default="50G", help='Max size of each sra file (default: 50G)')
+parser.add_argument('--outdir', '-o', default=os.getcwd(), help='Path to where to download .fastq files (default: currentdirectory')
 
 args = parser.parse_args()
+
+# Output directory to be created
+dir_name = "tmp_srahunter"
+
+# Check if directory already exists
+if not os.path.exists(dir_name):
+    # Create a new directory
+    os.mkdir(dir_name)
+    print(f"Directory '{dir_name}' created.")
+else:
+    print(f"Directory '{dir_name}' already exists.")
 
 # Validate the accession list file
 if not os.path.isfile(args.list):
@@ -67,13 +80,14 @@ for index, row in sra_numbers.iterrows():
 
     print("There is enough free space on the device. Continuing with the script.")
     print(f"Currently downloading: {sra_id}")
-    prefetch = f"prefetch -p -X {args.maxsize} {sra_id}"
+    prefetch = f"prefetch -p -X {args.maxsize} {sra_id} --output-file {args.path}/{sra_id}.sra"
     print(f"The command used was: {prefetch}")
     subprocess.call(prefetch, shell=True)
 
     print(f"Generating fastq for: {sra_id}")
    # Running fasterq-dump command
-    fasterq_dump_cmd = f"fasterq-dump --skip-technical -p -e {args.t} {args.path}/{sra_id}.sra"
+    fasterq_dump_cmd = f"fasterq-dump --skip-technical -p -e {args.t} {args.path}/{sra_id}.sra --outdir {args.outdir}"
+    print(f"The command used was: {fasterq_dump_cmd}")
     result = subprocess.call(fasterq_dump_cmd, shell=True)
 
     # Check if fasterq-dump was successful
@@ -87,10 +101,22 @@ for index, row in sra_numbers.iterrows():
         subprocess.call(rm_cmd, shell=True)
     else:
         print(f"Error in processing {sra_id}, skipping...")
-        with open("failed_list.csv", "a") as f:
-            f.write(sra_id + "\n")
+        with open(f"{args.path}/failed_list.csv", "a") as f:
+         f.write(sra_id + "\n")
 
 
-rm_temp = f"rm remind_list.csv"
-subprocess.call(rm_temp, shell=True)
+#Removing temporary files
 
+os.remove("remind_list.csv") 
+dir_path =args.path
+
+# Attempt to remove the directory and printing acknowledgments 
+try:
+    os.rmdir(dir_path)
+    print(pyfiglet.figlet_format("SRAHunter"))
+    print(f"All samples sucessful processed ")
+    print(f"Thank you for choosing SRAhunter please remember to cite our pubblication: or the github page ")
+except OSError as error:
+    print(pyfiglet.figlet_format("SRAHunter"))
+    print(f"Some samples were not properly downloaded check {args.path}/failed_list.csv")
+    print(f"Thank you for choosing SRAhunter please remember to cite our pubblication: or the github page ")
