9725ae073437ace9bafe2a86bd643ee99e15d9e4
Enrico <75318813+GitEnricoNeko@users.noreply.github.com>
2024-03-05 13:25:20 +0100

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,4 @@
+# srahunter/__init__.py
+from .scripts.download import main as download_main
+from .scripts.metadata import main as metadata_main
+from .scripts.fullmetadata import main as fullmetadata_main
--- setup.py
+++ setup.py
@@ -3,26 +3,39 @@ from setuptools import setup
 
 setup(
     name='srahunter',
-    version='1.0.0', 
-    author='Bortoletto Enrico',  
-    author_email='enricobortoletto30@gmail.com',  
-    description='A tool for processing SRA accessions',  
-    long_description=open('README.md').read(),  
-    long_description_content_type='text/markdown',  
-    url='https://github.com/GitEnricoNeko/SRAHunter',  
+    version='1.0.0',
+    author='Bortoletto Enrico',
+    author_email='enricobortoletto30@gmail.com',
+    description='A tool for processing SRA accessions',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/GitEnricoNeko/SRAHunter',
     install_requires=[
-        'pandas',
-        'numpy',
-        'psutil',
-        'argparse',
-        'pyfiglet'
+     'pandas>=2.0.2',
+     'psutil>=5.7',
+     'pyfiglet==1.0.2',
+     'requests==2.31.0',
+     'tqdm==4.66.1'
     ],
-    scripts=['scripts/srahunter-download'],
+    packages=['.scripts'],  # Explicitly specifying packages if find_packages() was missing subpackages
+    entry_points={
+        'console_scripts': [
+            'srahunter=cli:main',
+        ],
+    },
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python'
+        'Programming Language :: Python :: 3',  # Assuming your package is for Python 3
+        'Programming Language :: Python :: 3.6',  # Specify other versions as necessary
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Operating System :: OS Independent',
     ],
+    python_requires='>=3.6',  # Specify the minimum Python version required
 )
-
